775d5a3fbb847eff3eb202e8c45547af7f7ee822
fubiqi <bickeygg@Gmail.com>
2021-05-06 21:20:30 +0800

    [week04] jump game

---
+++ Week04/leetcode_jump_game_ii_test.go
@@ -0,0 +1,32 @@
+package Week04
+
+// 1. 动态规划
+// 2. greedy 贪心
+
+func jump1(nums []int) int {
+	dp := make([]int, len(nums), len(nums))
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 1; j <= nums[i]; j++ {
+			if i+j < len(nums) && (dp[i+j] == 0 || dp[i+j] > dp[i]+1) {
+				dp[i+j] = dp[i] + 1
+			}
+		}
+	}
+	return dp[len(nums)-1]
+}
+
+func jump2(nums []int) int {
+	farthest, jumpEnd, jumps := 0, 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		n := nums[i]
+		if i+n > farthest {
+			farthest = i + n
+		}
+		if jumpEnd == i {
+			jumps++
+			jumpEnd = farthest
+		}
+	}
+	return jumps
+
+}
